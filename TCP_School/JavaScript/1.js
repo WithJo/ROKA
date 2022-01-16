6d8297275d889a9c7daa9098643b9bc6cbed090f
조재용 <babnbabn@kakao.com>
2022-01-16 13:42:04 +0000

    22.01.16

---
+++ TCP_School/JavaScript/1.js
@@ -0,0 +1,27 @@
+function printDate(){
+	
+	document.getElementById("date").innerHTML=Date();
+	
+}
+
+function pr(){
+	
+	var num=10;
+	var str = "JavaScript";
+	document.getElementById("result").innerHTML=num+str
+	
+}
+
+function bl(){
+	
+	var firstNum = 10;
+	var secondNum=11;
+	document.getElementById("result").innerHTML=(firstNum==secondNum);
+}
+
+function sy(){
+	var sym = Symbol("javascript");
+	var symObj = Object(sym);
+	
+	document.getElementById("result").innerHTML=(typeof sym)+"<br>"+(typeof symObj)
+}
